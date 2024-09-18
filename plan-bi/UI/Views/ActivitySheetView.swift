ef2dbbd8afab246f5f374790b7a82dad72582f22
BenjaminSyntax <155439470+BenjaminSyntax@users.noreply.github.com>
2024-09-18 12:41:03 +0200

    Add sheets

---
+++ plan-bi/UI/Views/ActivitySheetView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivitySheetView.swift
+//  plan-bi
+//
+//  Created by Benjamin Moritz on 18.09.24.
+//
+
+import SwiftUI
+
+struct ActivitySheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivitySheetView()
+}
